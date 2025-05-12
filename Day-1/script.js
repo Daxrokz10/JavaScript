ec9d4cad4ca0cabdb464d545c40e56a688b420da
Daxrokz10 <dakshgagnani@gmail.com>
2025-05-12 13:22:04 +0530

    first commit

---
+++ Day-1/script.js
@@ -0,0 +1,12 @@
+document.write("<h1>Hello World</h1>");
+let a = 5;
+let b = 10;
+let name = "John Doe";
+document.write(`Hello, ${name}`);
+
+document.write("<br>",a + b);
+
+let names = ["Daksh", "John", "Doe"];
+for(let i = 0; i<names.length;i++){
+    document.write("<br>", names[i]);
+}
