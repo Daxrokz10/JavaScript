100497aebba079da57f079941dc81187d0ac0f37
Daxrokz10 <dakshgagnani@gmail.com>
2025-05-15 11:05:46 +0530

    type conversion issue

--- Marks Calculator/script.js
+++ Marks Calculator/script.js
@@ -1,38 +1,36 @@
-
-
 document.getElementById('marks-calc').addEventListener('click', () => {
-let nameInput = document.getElementById('name');
-let name = nameInput.value;
-let mathsInput = document.getElementById('marks1');
-let maths = mathsInput.value;
-let englishInput = document.getElementById('marks2');
-let english = englishInput.value;
-let scienceInput = document.getElementById('marks3');
-let science = scienceInput.value;
+    let nameInput = document.getElementById('name');
+    let name = nameInput.value;
+    let maths = parseFloat(document.getElementById('marks1').value);
+    let english = parseFloat(document.getElementById('marks2').value);
+    let science = parseFloat(document.getElementById('marks3').value);
 
-let avgMarks = (maths + english + science) / 3;
-let grade = '';
+    let totalMarks = maths + english + science;
+    let avgMarks = totalMarks / 3;
+    let grade = '';
 
-if(avgMarks >= 90){
-    grade = 'A+';
-}
-else if(avgMarks >= 80){
-    grade = 'A';
-}
-else if(avgMarks >= 70){
-    grade = 'B+';
-}
-else if(avgMarks >= 60){
-    grade = 'B';
-}
-else if(avgMarks >= 50){
-    grade = 'C+';
-}
-else if(avgMarks >= 40){
-    grade = 'C';
-}
-else if(avgMarks >= 33){
-    grade = 'D';
-}
+    if(avgMarks >= 90){
+        grade = 'A+';
+    }
+    else if(avgMarks >= 80){
+        grade = 'A';
+    }
+    else if(avgMarks >= 70){
+        grade = 'B+';
+    }
+    else if(avgMarks >= 60){
+        grade = 'B';
+    }
+    else if(avgMarks >= 50){
+        grade = 'C+';
+    }
+    else if(avgMarks >= 40){
+        grade = 'C';
+    }
+    else if(avgMarks >= 33){
+        grade = 'D';
+    } else {
+        grade = 'F';
+    }
     document.getElementById('marks-disp').innerText = grade;
 });
